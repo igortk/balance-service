0c9439df73518f24dcd9f49e1da7f1acc97a0d1d
Ткаченко Игорь <i.tkachenko@simcord.com>
2025-06-21 00:35:41 +0300

    add logs

--- services/server.go
+++ services/server.go
@@ -30,15 +30,19 @@ func NewServer2(pg *pg.Client, s *senders.Sender, c *amqp.Connection) *Server {
 }
 
 func (s *Server) Run(ctx context.Context, wg *sync.WaitGroup) {
+	log.Info("Starting server...")
 	//handlers
 	emitBalanceHandler := handlers.NewEmitBalanceByUserIdHandler(s.pgClient, s.sender)
 	getBalanceByUserIdHandler := handlers.NewGetBalanceByUserIdHandler(s.pgClient, s.sender)
+	log.Info("Handlers was prepared")
 
 	//consumers
 	s.consumers["GetBalanceByUserIdConsumer"] = consumers.NewConsumer(s.conn, config.RabbitBalanceExchange, config.GetBalanceByUserIdRequestRoutingKey, config.GetBalanceByUserIdRequestQueueName, getBalanceByUserIdHandler)
 	s.consumers["EmitBalanceByUserIdConsumer"] = consumers.NewConsumer(s.conn, config.RabbitBalanceExchange, config.EmitBalanceByUserIdRequestRoutingKey, config.EmitUserBalanceRequestQueueName, emitBalanceHandler)
+	log.Info("Consumers was prepared")
 
 	s.runAllConsumers(ctx, wg)
+	log.Info("Server is running...")
 
 	select {
 	case <-ctx.Done():
